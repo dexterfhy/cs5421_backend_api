327089581972d2f94b542f3bcef967cfb015d157
Dexter Fong <dexterfonghy@gmail.com>
2022-03-29 17:58:09 +0800

    Handle failure of all attempted cases for single attempt completion event

--- api_app/consumer.py
+++ api_app/consumer.py
@@ -65,30 +65,38 @@ def process_job_init_completion(msg):
 
 def process_job_attempt_completion(msg):
     try:
-        attempted_case = AttemptedCase.objects.get(attempt_id=msg.value['attempt_id'],
-                                                   test_case_id=msg.value['test_case_id'])
+        if msg.value['test_case_id']:
+            attempted_case = AttemptedCase.objects.get(attempt_id=msg.value['attempt_id'],
+                                                       test_case_id=msg.value['test_case_id'])
 
-        status = msg.value['status'] if 'status' in msg.value else 'PENDING'
-        execution_ms = msg.value['execution_ms'] if 'execution_ms' in msg.value else 0
-        if 'CORRECT' == status:
-            test_case = TestCaseSerializer.objects.get(id=msg.value['test_case_id'])
-            actual_result = test_case.expected_result
+            update_attempted_case(msg, attempted_case)
         else:
-            actual_result = msg.value['actual_result'] if 'actual_result' in msg.value else '{}'
+            attempted_cases = AttemptedCase.objects.filter(attempt_id=msg.value['attempt_id'])
 
-        data = dict({
-            'status': status,
-            'execution_ms': execution_ms,
-            'actual_result': actual_result
-        })
-        attempted_case_serializer = AttemptedCaseSerializer(attempted_case, data=data, partial=True)
-
-        if attempted_case_serializer.is_valid():
-            attempted_case_serializer.save()
-        else:
-            print("Invalid fields for updating attempt with job completion event {0}".format(msg))
+            for attempted_case in attempted_cases:
+                update_attempted_case(msg, attempted_case)
     except ObjectDoesNotExist:
         print("Unable to update attempt with job completion event {0}".format(msg))
+
+
+def update_attempted_case(msg, attempted_case):
+    status = msg.value['status'] if 'status' in msg.value else 'PENDING'
+    execution_ms = msg.value['execution_ms'] if 'execution_ms' in msg.value else 0
+    if 'CORRECT' == status:
+        test_case = TestCaseSerializer.objects.get(id=msg.value['test_case_id'])
+        actual_result = test_case.expected_result
+    else:
+        actual_result = msg.value['actual_result'] if 'actual_result' in msg.value else '{}'
+    data = dict({
+        'status': status,
+        'execution_ms': execution_ms,
+        'actual_result': actual_result
+    })
+    attempted_case_serializer = AttemptedCaseSerializer(attempted_case, data=data, partial=True)
+    if attempted_case_serializer.is_valid():
+        attempted_case_serializer.save()
+    else:
+        print("Invalid fields for updating attempt with job completion event {0}".format(msg))
 
 
 consumer_thread = threading.Thread(target=handle_job_completion_events)
